119712784bd3eb6cab3eed8d63a6c608efd464c1
CarrivasUclm <carlos.rivas1@alu.uclm.es>
2020-10-07 12:21:26 +0200

    Launcher

---
+++ Proyecto4/src/main/java/com/agibilibus/listaToDo/model/Manager.java
@@ -0,0 +1,25 @@
+package com.agibilibus.listaToDo.model;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+
+
+@Component
+public class Manager {
+
+	@Bean
+	public static Manager get() {
+		return ManagerHolder.singleton;
+	}
+	
+	private static class ManagerHolder {
+		static Manager singleton = new Manager();
+	}
+	
+	private Manager() {
+		
+		
+	}
+
+}
--- Proyecto4/src/main/java/com/agibilibus/listaToDo/model/Tarea.java
+++ Proyecto4/src/main/java/com/agibilibus/listaToDo/model/Tarea.java
@@ -1,32 +1,31 @@
 package com.agibilibus.listaToDo.model;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
 import com.agibilibus.listaToDo.dao.TareaDAO;
 
 public class Tarea {
-	
+
 	private String id;
 	private String nombre;
 	private boolean done;
-	
-	public Tarea() {}
-	public Tarea (String nombre, boolean done) {
+
+	public Tarea() {
+	}
+
+	public Tarea(String nombre, boolean done) {
 		this.nombre = nombre;
 		this.done = done;
 	}
-	
-	
-	
+
 	public String getId() {
 		return id;
 	}
 
-
-
 	public void setId(String id) {
 		this.id = id;
 	}
-
-
 
 	public String getNombre() {
 		return nombre;
@@ -43,10 +42,9 @@ public class Tarea {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	
+
 	public boolean select() {
 		return new TareaDAO().select(this);
 	}
-	
 
 }
