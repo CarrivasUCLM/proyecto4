88602bd2afc1c6e714da57da3f97ae2ff480650e
CarrivasUclm <carlos.rivas1@alu.uclm.es>
2020-10-13 01:40:30 +0200

    Buenas noches

--- Proyecto4/src/main/webapp/ToDoList.js
+++ Proyecto4/src/main/webapp/ToDoList.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	loadTasks();
 	$('#new').click(function() {
 		agregarFila();
 	});
@@ -51,5 +52,43 @@ function addTarea(fila) {
 }
 
 function loadTasks() {
+	var data = {
 
+			url : "loadTareas",
+			type : "post",
+			
+			success : function(response) {
+				var options ="";
+				for (var i=0; i<response.length; i++) {
+					options = options + "<tr><td>" + response[i].nombre + "</td></tr>";
+				}
+				document.getElementById('cuerpo').innerHTML = options;
+			},
+			error : function(response) {
+				alert(response.message);
+			}
+		};
+		$.ajax(data);
+}
+
+function deleteTarea() {
+	var info = {
+		type : "DeleteTarea",
+		tarea : tareaName.value,
+		done : checkDone.value
+	};
+	var data = {
+		data : JSON.stringify(info),
+		url : "deletetarea",
+		type : "post",
+		contentType : 'application/json',
+		dataType : 'json',
+		success : function() {
+			alert("OK");
+		},
+		error : function(response) {
+			alert(response.responseText);
+		}
+	};
+	$.ajax(data);
 }
